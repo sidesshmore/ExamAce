60b8d5d46d96d03e559615467ba0a7fe1d51588e
Sidessh <mosiddhesh@gmail.com>
2024-09-11 13:04:12 +0530

    chat interface started

--- lib/features/Home/ui/home.dart
+++ lib/features/Home/ui/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,15 +11,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  ChatUser sidessh = ChatUser(id: '1', firstName: 'Sidessh');
+  ChatUser aquaBot =
+      ChatUser(id: '2', firstName: 'ExamAce', profileImage: 'assets/icon.png');
+
+  List<ChatMessage> messageList = [];
+  List<ChatUser> typingAnimation = [];
+
+  final header = {'Content-Type': 'application/json'};
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Center(
-        child: Text('Coming soon..',
-            style: TextStyle(
-                fontSize: screenWidth * 0.13, fontWeight: FontWeight.w600)),
+    return const Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [],
+        ),
       ),
     );
   }
