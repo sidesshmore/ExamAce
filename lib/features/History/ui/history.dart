3a138446c752745e9479cb70ad1407e9b94dca3e
Sidessh <mosiddhesh@gmail.com>
2024-09-11 12:52:50 +0530

    history screen created

--- lib/features/History/ui/history.dart
+++ lib/features/History/ui/history.dart
@@ -1,3 +1,4 @@
+import 'package:examace/features/MainScreen/ui/mainScreen.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatefulWidget {
@@ -12,11 +13,23 @@ class _HistoryState extends State<History> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Center(
-        child: Text('History',
-            style: TextStyle(
-                fontSize: screenWidth * 0.13, fontWeight: FontWeight.w600)),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new_rounded),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Mainscreen()),
+              );
+            },
+          ),
+          title: Text('History',
+              style: TextStyle(
+                  fontSize: screenWidth * 36 / 432,
+                  fontWeight: FontWeight.w400)),
+        ),
       ),
     );
   }
