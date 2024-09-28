909b9b1f4802f8320672361d5931caa153b1d2ee
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-09-28 21:43:20 +0530

    firebase signup resolved

--- lib/features/Signup/repo/signupRepo.dart
+++ lib/features/Signup/repo/signupRepo.dart
@@ -7,9 +7,9 @@ class SignupRepo {
   static Future<User?> register(String name,String email,String password)async{
     FirebaseAuth auth=FirebaseAuth.instance;
     try {
-      UserCredential credential=await auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential=await auth.signInWithEmailAndPassword(email: email, password: password);
       await credential.user?.updateDisplayName(name);
-      print(credential.toString());
+      log(credential.toString());
       return credential.user;
       
     } catch (e) {
